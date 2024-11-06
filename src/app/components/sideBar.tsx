88ab7daa814632cf29f4edc05ca831aabecee822
Simple134 <sanchezjosue2001@gmail.com>
2024-11-06 10:11:58 -0400

    Component 3 changes
    Cambio al componente del imagegrid 3

--- src/app/components/sideBar.tsx
+++ src/app/components/sideBar.tsx
@@ -5,24 +5,26 @@ import React from 'react';
 const Sidebar = () => {
 
     return (
-        <div className="pl-6 absolute left-0 w-[7%] h-[99vh]  bg-white shadow-lg p-4 z-50 overflow-y-auto space-y-2 pb-8 no-scrollbar">
-            <div className=' w-16 h-16 border-2 border-black rounded-full'>
-            </div>
-            <p className='text-center text-gray-500'>Bakery</p>
-            <div className=' w-16 h-16 border-2 border-black rounded-full'></div>
-            <p className='text-center text-gray-500'>Deli</p>
-            <div className=' w-16 h-16 border-2 border-black rounded-full'></div>
-            <p className='text-center text-gray-500'>Dairy</p>
-            <div className=' w-16 h-16 border-2 border-black rounded-full'></div>
-            <p className='text-center text-gray-500'>Frozen</p>
-            <div className=' w-16 h-16 border-2 border-black rounded-full'></div>
-            <p className='text-center text-gray-500'>Grocery</p>
-            <div className=' w-16 h-16 border-2 border-black rounded-full'></div>
-            <p className='text-center text-gray-500'>Meat</p>
-            <div className=' w-16 h-16 border-2 border-black rounded-full'></div>
-            <p className='text-center text-gray-500'>Seafood</p>
-            <div className=' w-16 h-16 border-2 border-black rounded-full'></div>
-            <p className='text-center text-gray-500'>Liquor</p>
+        <div className="flex flex-col justify-center items-center p-2 absolute left-0  bg-white shadow-lg p-4 z-50 overflow-y-auto space-y-2 pb-8 no-scrollbar">
+            <button className=' w-20 h-20 border-2 border-black rounded-full cursor-pointer' onClick={() => {
+                window.alert('Bakery');
+            }}>
+            </button>
+            <p className='text-gray-500'>Bakery</p>
+            <button className='w-20 h-20 border-2 border-black rounded-full cursor-pointer'></button>
+            <p className='text-gray-500'>Deli</p>
+            <button className=' w-20 h-20 border-2 border-black rounded-full cursor-pointer'></button>
+            <p className='text-gray-500'>Dairy</p>
+            <button className=' w-20 h-20 border-2 border-black rounded-full cursor-pointer'></button>
+            <p className='text-gray-500'>Frozen</p>
+            <button className=' w-20 h-20 border-2 border-black rounded-full cursor-pointer'></button>
+            <p className='text-gray-500'>Grocery</p>
+            <button className=' w-20 h-20 border-2 border-black rounded-full cursor-pointer'></button>
+            <p className='text-gray-500'>Meat</p>
+            <button className=' w-20 h-20 border-2 border-black rounded-full cursor-pointer'></button>
+            <p className='text-gray-500'>Seafood</p>
+            <button className=' w-20 h-20 border-2 border-black rounded-full cursor-pointer'></button>
+            <p className='text-gray-500'>Liquor</p>
         </div>
     );
 };
