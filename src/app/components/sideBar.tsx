e49710bfc48b472d117846984556202a68c4cee3
Simple134 <sanchezjosue2001@gmail.com>
2024-11-07 16:03:46 -0400

    More updates to the website

--- src/app/components/sideBar.tsx
+++ src/app/components/sideBar.tsx
@@ -9,50 +9,50 @@ import { BakeryIcon, DairyIcon, DeliIcon, FrozenIcon, GroceryIcon, LiquorIcon, M
 const Sidebar = ({ onCategorySelect }: SidebarProps) => {
 
     return (
-        <div className="flex flex-col justify-center items-center p-2 absolute left-0  bg-white shadow-lg p-4 z-50 overflow-y-auto space-y-2 pb-8 no-scrollbar">
-            <button className=' w-20 h-20 border-2 border-[606060] rounded-full cursor-pointer flex items-center justify-center' id="bakery" onClick={() => {
+        <div className="flex flex-col justify-center items-center p-2 absolute left-0  bg-white shadow-lg p-4 z-50 overflow-y-auto space-y-2 pb-8 no-scrollbar sm:hidden">
+            <button className=' w-20 h-20 border-2 border-[#606060] hover:border-[#7cc304] rounded-lg cursor-pointer flex items-center justify-center' id="bakery" onClick={() => {
                 onCategorySelect('bakery');
             }}>
                 <BakeryIcon />
             </button>
             <p className='text-gray-500'>Bakery</p>
-            <button className='w-20 h-20 border-2 border-[606060] rounded-full cursor-pointer flex items-center justify-center' id="deli" onClick={() => {
+            <button className='w-20 h-20 border-2 border-[#606060] rounded-lg cursor-pointer flex items-center justify-center' id="deli" onClick={() => {
                 onCategorySelect('deli');
             }}>
                   <DeliIcon />
             </button>
             <p className='text-gray-500'>Deli</p>
-            <button className=' w-20 h-20 border-2 border-[606060] rounded-full cursor-pointer flex items-center justify-center' id="dairy" onClick={() => {
+            <button className=' w-20 h-20 border-2 border-[#606060] rounded-lg cursor-pointer flex items-center justify-center' id="dairy" onClick={() => {
                 onCategorySelect('dairy');
             }}>
                 <DairyIcon />
             </button>
             <p className='text-gray-500'>Dairy</p>
-            <button className=' w-20 h-20 border-2 border-[606060] rounded-full cursor-pointer flex items-center justify-center' id="frozen" onClick={() => {
+            <button className=' w-20 h-20 border-2 border-[#606060] rounded-lg cursor-pointer flex items-center justify-center' id="frozen" onClick={() => {
                 onCategorySelect('frozen');
             }}>
                 <FrozenIcon />
             </button>
             <p className='text-gray-500'>Frozen</p>
-            <button className=' w-20 h-20 border-2 border-[606060] rounded-full cursor-pointer flex items-center justify-center' id="grocery" onClick={() => {
+            <button className=' w-20 h-20 border-2 border-[#606060] rounded-lg cursor-pointer flex items-center justify-center' id="grocery" onClick={() => {
                 onCategorySelect('grocery');
             }}>
                 <GroceryIcon />
             </button>
             <p className='text-gray-500'>Grocery</p>
-            <button className=' w-20 h-20 border-2 border-[606060] rounded-full cursor-pointer flex items-center justify-center' id="meat" onClick={() => {
+            <button className=' w-20 h-20 border-2 border-[#606060] rounded-lg cursor-pointer flex items-center justify-center' id="meat" onClick={() => {
                 onCategorySelect('meat');
             }}>
                 <MeatIcon />
             </button>
             <p className='text-gray-500'>Meat</p>
-            <button className=' w-20 h-20 border-2 border-[606060] rounded-full cursor-pointer flex items-center justify-center' id="seafood" onClick={() => {
+            <button className=' w-20 h-20 border-2 border-[#606060] rounded-lg cursor-pointer flex items-center justify-center' id="seafood" onClick={() => {
                 onCategorySelect('seafood');
             }}>
                 <SeafoodIcon />
             </button>
             <p className='text-gray-500'>Seafood</p>
-            <button className=' w-20 h-20 border-2 border-[606060] rounded-full cursor-pointer flex items-center justify-center' id="liquor" onClick={() => {
+            <button className=' w-20 h-20 border-2 border-[#606060] rounded-lg cursor-pointer flex items-center justify-center' id="liquor" onClick={() => {
                 onCategorySelect('liquor');
             }}>
                 <LiquorIcon />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/header";
 import { ProductProvider } from "./context/productContext";
+import { PrimeReactProvider } from "primereact/api";
+import Tailwind from 'primereact/passthrough/tailwind';
 
 
 export const metadata: Metadata = {
@@ -17,10 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+      <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
       <ProductProvider>
           <Header />
           {children}
         </ProductProvider>
+        </PrimeReactProvider>
       </body>
     </html>
   );
