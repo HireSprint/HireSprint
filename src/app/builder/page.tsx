4efc217ee477debd430916c2731a994d04008cb9
vjose1903 <vjose1903@outlook.es>
2024-11-29 16:39:25 -0400

    iniciando el layout del grid builder

---
+++ src/app/builder/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+
+import React, { useState } from "react";
+
+
+const BuilderPage = () => {
+    return (
+        <div className="w-full h-full grid grid-cols-[min-content_1fr]">
+            
+            <div className="bg-gray-500 w-[300px] h-full">
+                dfdfdf
+            </div>
+
+            <div className="">
+            
+            </div>
+        </div>
+    )
+}
+
+export default BuilderPage;
